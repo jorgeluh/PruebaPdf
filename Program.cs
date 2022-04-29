2f2f7133381b38fce26722185f73f455eddeb8fc
Jorge Raúl Lu Hernández <jorge.lu.h@hotmail.com>
2022-04-28 21:33:49 -0600

    Versión inicial. Detección básica de firma en archivo usando PdfPig.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace PruebaPdf
+{
+    using System;
+    using System.Linq;
+    using UglyToad.PdfPig;
+    using UglyToad.PdfPig.AcroForms;
+    using UglyToad.PdfPig.AcroForms.Fields;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("¿Archivo? ");
+            string archivo = Console.ReadLine();
+
+            using (PdfDocument documento = PdfDocument.Open(archivo))
+            {
+                AcroForm formulario;
+                bool contieneFormularios = documento.TryGetForm(out formulario);
+                if (contieneFormularios && formulario.Fields.Any(f => f.FieldType == AcroFieldType.Signature))
+                {
+                    Console.WriteLine("Tiene firmas.");
+                }
+                else
+                {
+                    Console.WriteLine("No tiene firmas.");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
